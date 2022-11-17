4b519632225fb49f89679cf186d27f8ce2ae5c7a
roserkan <serkannrose@gmail.com>
2022-11-17 16:11:01 +0300

    elastic search added.

---
+++ src/corePackages/Core.ElasticSearch/Models/ElasticSearchModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.ElasticSearch.Models
+{
+    public class ElasticSearchModel
+    {
+        public Id ElasticId { get; set; }
+        public string IndexName { get; set; }
+    }
+}
